14c66c0468607cc06179326209b0e16e041d145c
dawirashid <77828860+dawirashid@users.noreply.github.com>
2021-12-17 20:19:01 +0800

    add BottomNavigationBar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,6 +10,24 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return   Scaffold(
+      body: const Center(
+        child: Text(
+          'Hello',
+          style:  TextStyle(fontSize: 50),
+        ),
+      ),
+      backgroundColor:  const Color(0xE88997D2),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blueGrey.shade100,
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message'),
+        BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Account'),
+        BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Home'),
+      ],
+      ),
+    );
   }
 }
